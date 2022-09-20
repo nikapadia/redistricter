e2f5534c15d44961aa39470e630124f2a89d5d2c
nikapadia <nilayrkapadia@gmail.com>
2022-09-19 23:09:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Redistricter
+
+A hobby project with the goal of creating a website that will create congressional districts as fair as possible. Not sure how that is going to work, but that's the goal. For now the website will only create districts for New York. If I can get one state working in a resonable timeframe, I'll work on other states.
+
+Built by Nilay Kapadia using Nuxt/Vue.
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <div class="header">
+      <h1>Redistricter</h1>
+      <h2>Because Redistricting Sucks</h2>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+  h1 {
+    font-family: 'Space Mono', monospace;
+    font-weight: 700;
+    font-size: 100px;
+    color: #3791ff;
+    margin: 0;
+  }
+  h2 {
+    font-family: 'Space Mono', monospace;
+    font-weight: 400;
+    font-size: 40px;
+    font-style: italic;
+    margin: 0;
+  }
+  .header {
+    text-align: center;
+  }
+</style>
